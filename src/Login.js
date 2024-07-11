0a5e80dbf7a57093562acca7d018f15c938db0a2
Anushree1291 <anushree12september@gmail.com>
2024-07-11 18:58:28 +0530

    changes

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css'
+
+function login() {
+    return (
+        <div className="login">
+            {/* spotify logo  */}
+            {/* login with spo button  */}
+        </div>
+    );
+}
+
+export default login;
